f2184e7fa8e12a879421c643395b8141f59b2cac
winter757 <114052140+winter757@users.noreply.github.com>
2023-03-01 10:25:06 +0200

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -35,12 +35,21 @@ def SaveContacts():
     with open('prog1-10-0810/contacts.json', 'w', encoding='UTF8') as file:
         contacts_dict = {'contacts_list': contacts}
         json.dump(contacts_dict, file, indent=4)
+        
+def DeleteContact():
+    contact_name = input("Enter the name of the contact you want to delete: ")
+    for i, contact in enumerate(contacts):
+        if contact["name"] == contact_name:
+            contacts.pop(i)
 
 LoadContacts()
 while(True):
-    response = input('A-add new, P-print, E-exit: ')
+    response = input('A-add new, D-delete, P-print, E-exit: ')
     if response == 'A':        
         AddContact()
+        SaveContacts()
+    elif response == "D":
+        DeleteContact()
         SaveContacts()
     elif response == 'P':
         PrintContacts()
