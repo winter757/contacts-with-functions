42e84a12c587ae3ccb5b6e9aeeb83ab243465bbe
winter757 <114052140+winter757@users.noreply.github.com>
2023-03-08 10:28:29 +0200

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,58 +1,75 @@
-# IZVEIDOSIM KONTAKTU APLIKĀCIJU IZMANTOJOT:
-# Funkcijas
-# Datu struktūras (JSON)
-# File I/O
 import json
+import csv
 
 contacts = []
 
-def LoadContacts():    
-    with open('prog1-10-0810/contacts.json', 'r', encoding='UTF8') as file:        
-        data = json.load(file)
-        global contacts
-        contacts = data['contacts_list']    
-
 def PrintContacts():
     for contact in contacts:
-        print(f"{contact['name']} {contact['surname']} {contact['number']} {contact['email']}")
+            print(f"{contact['name']} {contact['surname']} {contact['number']} {contact['email']}")
 
 def AddContact():
-    name = input('Enter name: ')
-    surname = input('Enter surname: ')
-    number = input('Enter number: ')
-    email = input('Enter email: ')
+    name = input("Enter name: ")
+    surname = input("Enter surname: ")
+    number = input("Enter number: ")
+    email = input("Enter email:")
 
     contact = {
-        'name': name, 
-        'surname': surname, 
-        'number': number,
-        'email': email
-    }
+        "name": name, 
+        "surname": surname, 
+        "number": number,
+        "email": email
+        }
 
     contacts.append(contact)
 
-def SaveContacts():
-    with open('prog1-10-0810/contacts.json', 'w', encoding='UTF8') as file:
+def SaveContacts(format):
+        if format == "json":
+             SaveAsJson()
+        elif format == "csv":
+             SaveAsCsv()
+        else:
+             print("File not saved. File format not defined.")
+
+def SaveAsCsv():
+    with open("contacts.csv", "w") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=virsraksti)
+            writer.writeheader()
+            writer.writerows(contacts)
+
+def SaveAsJson():
+    with open("contacts.json", "w", encoding="UTF8") as file:
         contacts_dict = {'contacts_list': contacts}
         json.dump(contacts_dict, file, indent=4)
-        
-def DeleteContact():
-    contact_name = input("Enter the name of the contact you want to delete: ")
-    for i, contact in enumerate(contacts):
-        if contact["name"] == contact_name:
-            contacts.pop(i)
 
-LoadContacts()
-while(True):
-    response = input('A-add new, D-delete, P-print, E-exit: ')
-    if response == 'A':        
-        AddContact()
-        SaveContacts()
+virsraksti = ["name", "surname", "number", "email"]
+
+def DeleteContact():
+    name = input('Enter name: ')    
+    found = False
+
+    for contact in contacts:
+        if contact['name'] == name:
+            contacts.remove(contact)
+            found = True
+            break
+
+    if found:
+        print(f"Contact {name} has been deleted.")
+    else:
+        print(f"Contact {name} not found.")
+
+while True:
+    response = input("A - add new, P - print, D - delete, C - save as csv, E - exit: ")
+    if response == "A":
+         AddContact()
+         SaveContacts("json")
+    elif response == "C":
+         SaveContacts("csv")
+    elif response == "P":
+         PrintContacts()
     elif response == "D":
         DeleteContact()
-        SaveContacts()
-    elif response == 'P':
-        PrintContacts()
-    elif response == 'E':
-        print('Bye bye!')
+    elif response == "E":
         break
+    else:
+        print("Please enter a valid response!")
